bcb0a6a80f878111fffcfd2fe65bde1ebe4acb3a
Tim Whitacre <tim@timw.co>
2015-10-20 12:45:21 -0400

    Reworked imports

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,13 @@
 // Constructors
 // Only add code to *THIS* section!
 
-// ????????
-// ????????
-// ????????
+///
+///
+///
+///
 
 // Do not ADD or MODIFY code below this line :)
 // Dogs
-
 'use strict';
 
 var sadie = new Dog({
@@ -23,11 +23,62 @@ var moonshine = new Dog({
 var atticus = new Dog();
 
 // Humans
-
 var mason = new Human();
 
 var julia = new Human({
   cool: true
+});
+
+var it = function it(description, contents) {
+  console.log('\n\n"It ' + description + '"');
+  contents();
+};
+
+var expect = function expect(target) {
+  return {
+    toBe: function toBe(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
+        return false;
+      }
+    }
+  };
+};
+
+// Tests
+it("should make Sadie happy when Mason pets her", function () {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function () {
+  expect(sadie.color).toBe('black');
+});
+
+it("should be make Moonshine hungry and Sadie not hungry", function () {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function () {
+  julia.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function () {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function () {
+  sadie.owner = mason;
+  expect(julia.cool).toBe(true);
+  expect(mason.cool).toBe(false);
 });
 
 },{}]},{},[1])
--- js/main.js
+++ js/main.js
@@ -1,29 +1,81 @@
 // Constructors
 // Only add code to *THIS* section!
 
-// ????????
-// ????????
-// ????????
+///
+///
+///
+///
 
 // Do not ADD or MODIFY code below this line :)
 // Dogs
-
-var sadie = new Dog({
+let sadie = new Dog({
   color: 'black',
   hungry: false,
 });
 
-var moonshine = new Dog({
+let moonshine = new Dog({
   color: 'blue-red',
 });
 
-var atticus = new Dog();
-
+let atticus = new Dog();
 
 // Humans
+let mason = new Human();
 
-var mason = new Human();
-
-var julia = new Human({
+let julia = new Human({
   cool: true,
+});
+
+
+let it = function (description, contents) {
+  console.log('\n\n"It ' + description + '"');
+  contents();
+};
+
+let expect = function (target) {
+  return {
+    toBe: function(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
+        return false;
+      }
+    }
+  };
+};
+
+// Tests
+it("should make Sadie happy when Mason pets her", function() {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function(){
+  expect(sadie.color).toBe('black');
+});
+
+it("should be make Moonshine hungry and Sadie not hungry", function() {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function() {
+  julia.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function() {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function() {
+  sadie.owner = mason;
+  expect(julia.cool).toBe(true);
+  expect(mason.cool).toBe(false);
 });
