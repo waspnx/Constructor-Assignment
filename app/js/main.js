dbdddfed2792265aa10e8b53ab0cab506191fdf2
Tim Whitacre <tim@timw.co>
2015-10-20 10:31:51 -0400

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Constructors
+// Only add code to *THIS* section!
+
+// ????????
+// ????????
+// ????????
+
+// Do not ADD or MODIFY code below this line :)
+// Dogs
+
+'use strict';
+
+var sadie = new Dog({
+  color: 'black',
+  hungry: false
+});
+
+var moonshine = new Dog({
+  color: 'blue-red'
+});
+
+var atticus = new Dog();
+
+// Humans
+
+var mason = new Human();
+
+var julia = new Human({
+  cool: true
+});
+
+},{}]},{},[1])
+
+
+//# sourceMappingURL=main.js.map
---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Constructors
+// Only add code to *THIS* section!
+
+// ????????
+// ????????
+// ????????
+
+// Do not ADD or MODIFY code below this line :)
+// Dogs
+
+var sadie = new Dog({
+  color: 'black',
+  hungry: false,
+});
+
+var moonshine = new Dog({
+  color: 'blue-red',
+});
+
+var atticus = new Dog();
+
+
+// Humans
+
+var mason = new Human();
+
+var julia = new Human({
+  cool: true,
+});
